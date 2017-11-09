ed0abad3b26220f1f933f91756801a83610d3072
AcuaroTorres <alvarotorres@gmail.com>
2017-11-09 17:06:24 -0300

    Migraciones, Modelos y Relaciones

---
+++ database/migrations/2017_11_09_184032_add_articles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            // title, content, cotegory_id, user_id
+            $table->string('title');
+            $table->text('content');
+            $table->integer('category_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
